2b783563506e40d23c13e9fff2f0c1d653c8be4f
aben-ham <ayoubbenhamou0731@gmail.com>
2023-03-02 17:24:35 +0100

    Push

--- public/main.py
+++ public/main.py
@@ -1,5 +1,6 @@
 import sys
 print("Content-Type: text/html", end="\r\n")
+print("Set-Cookie: name=ayoub;", end="\r\n")
 #print("Location: https://www.google.com", end="\r\n")
 #print("Status: 301 Not Found", end="\r\n")
 print(end="\r\n")
